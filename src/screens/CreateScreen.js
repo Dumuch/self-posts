af5701ed332ab9cc1c17c42f6178e5ce1fe18501
Dumuch <dumuch@yandex.ru>
2022-01-14 10:58:00 +0800

    summary

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef } from "react";
 import {
   View,
   Text,
@@ -8,34 +8,34 @@ import {
   Button,
   ScrollView,
   TouchableWithoutFeedback,
-  Keyboard
-} from 'react-native'
-import { useDispatch } from 'react-redux'
-import { HeaderButtons, Item } from 'react-navigation-header-buttons'
-import { AppHeaderIcon } from '../components/AppHeaderIcon'
-import { THEME } from '../theme'
-import { addPost } from '../store/actions/post'
-import { PhotoPicker } from '../components/PhotoPicker'
+  Keyboard,
+} from "react-native";
+import { useDispatch } from "react-redux";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { AppHeaderIcon } from "../components/AppHeaderIcon";
+import { THEME } from "../theme";
+import { addPost } from "../store/actions/post";
+import { PhotoPicker } from "../components/PhotoPicker";
 
 export const CreateScreen = ({ navigation }) => {
-  const dispatch = useDispatch()
-  const [text, setText] = useState('')
-  const imgRef = useRef()
+  const dispatch = useDispatch();
+  const [text, setText] = useState("");
+  const imgRef = useRef();
 
   const saveHandler = () => {
     const post = {
       date: new Date().toJSON(),
       text: text,
-      img: imgRef.current,
-      booked: false
-    }
-    dispatch(addPost(post))
-    navigation.navigate('Main')
-  }
+      // img: imgRef.current,
+      booked: false,
+    };
+    dispatch(addPost(post));
+    navigation.navigate("Main");
+  };
 
-  const photoPickHandler = uri => {
-    imgRef.current = uri
-  }
+  const photoPickHandler = (uri) => {
+    imgRef.current = uri;
+  };
 
   return (
     <ScrollView>
@@ -44,14 +44,14 @@ export const CreateScreen = ({ navigation }) => {
           <Text style={styles.title}>Создай новый пост</Text>
           <TextInput
             style={styles.textarea}
-            placeholder='Введите текст заметки'
+            placeholder="Введите текст заметки"
             value={text}
             onChangeText={setText}
             multiline
           />
           <PhotoPicker onPick={photoPickHandler} />
           <Button
-            title='Создать пост'
+            title="Создать пост"
             color={THEME.MAIN_COLOR}
             onPress={saveHandler}
             disabled={!text}
@@ -59,34 +59,34 @@ export const CreateScreen = ({ navigation }) => {
         </View>
       </TouchableWithoutFeedback>
     </ScrollView>
-  )
-}
+  );
+};
 
 CreateScreen.navigationOptions = ({ navigation }) => ({
-  headerTitle: 'Создать пост',
+  headerTitle: "Создать пост",
   headerLeft: (
     <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
       <Item
-        title='Toggle Drawer'
-        iconName='ios-menu'
+        title="Toggle Drawer"
+        iconName="ios-menu"
         onPress={() => navigation.toggleDrawer()}
       />
     </HeaderButtons>
-  )
-})
+  ),
+});
 
 const styles = StyleSheet.create({
   wrapper: {
-    padding: 10
+    padding: 10,
   },
   title: {
     fontSize: 20,
-    textAlign: 'center',
-    fontFamily: 'open-regular',
-    marginVertical: 10
+    textAlign: "center",
+    fontFamily: "open-regular",
+    marginVertical: 10,
   },
   textarea: {
     padding: 10,
-    marginBottom: 10
-  }
-})
+    marginBottom: 10,
+  },
+});
